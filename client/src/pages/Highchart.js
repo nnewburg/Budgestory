b4760409d985e1a445b007ae08706350c5db4ce8
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-29 18:48:59 +0000

    Major functions relevant to the main chart

---
+++ client/src/pages/Highchart.js
@@ -0,0 +1,277 @@
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import axios from 'axios';
+import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts';
+import Drilldown from 'highcharts/modules/drilldown';
+// check if HighchartsDrilldown has already been loaded
+if (!Highcharts.Chart.prototype.addSeriesAsDrilldown) {
+    Drilldown(Highcharts);
+}
+
+let chartOptions = {
+  chart: {
+    type: 'pie'
+  },
+  title: {
+    text: 'Hao Jiang Balance:$100,000,000 January, 2020'
+  },
+  // subtitle: {
+  //     text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+  // },
+  // credits: {
+  //   enabled: true
+  // },
+  // xAxis: {
+  //   categories: ["Expenses", "Incomes"]
+  // },
+  plotOptions: {
+    series: {
+      dataLabels: {
+        enabled: true,
+        format: '{point.name}: ${point.v:.1f}, {point.y:.1f}%'
+      },
+      cursor: 'pointer',
+      point: {
+        events: {
+          click: function (e) {
+              // alert('clicked! ' + this.state);
+            // console.log(this);
+          }
+        }
+      }
+    }
+  },
+  tooltip: {
+    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+  },
+  series: [
+    {
+      name: "Balance",
+      id: "Balance",
+      // colorByPoint: true,
+      data: [
+        { name: "Expenses", y: 60, v: 600, drilldown: 1},
+        { name: "Incomes", y: 40, v: 400, drilldown: 2}
+      ]
+    }
+  ],
+  drilldown: {
+    series: [
+      {
+        name: "Expenses",
+        id: 1,
+        data: [
+          { name: "Clothes", y: 70, v: 700, drilldown: 3},
+          { name: "Dining Out", y: 30, v: 300, drilldown: 4}
+        ]
+      },
+      {
+        name: "Incomes",
+        id: 2,
+        data: [
+          { name: "Salary", y: 90, v: 9000, drilldown: null},
+          { name: "Bonus", y: 10, v: 1000, drilldown: null}
+        ]
+      },
+      {
+        name: "Clothes",
+        id: 3,
+        data: [
+          { name: "Clothes1", y: 10, v: 100, drilldown: null},
+          { name: "Clothes2", y: 20, v: 200, drilldown: null}, 
+          { name: "Clothes3", y: 30, v: 300, drilldown: null},
+          { name: "Clothes4", y: 40, v: 400, drilldown: null}
+        ]
+      },
+      {
+        name: "Dining Out",
+        id: 4,
+        data: [
+          { name: "Food1", y: 20, v: 200},
+          { name: "Food2", y: 30, v: 300},
+          { name: "Food3", y: 50, v: 500}
+        ]
+      }
+    ]          
+  }
+};
+
+// var chart = Highcharts.chart('MajorChart', {
+//   series: [{
+//       type: 'column',
+//       data: [{
+//           y: 2,
+//           drilldown: 'd1'
+//       }]
+//   }],
+
+//   drilldown: {
+//       series: [{
+//           type: 'column',
+//           data: [1,2,3,4],
+//           id: 'd1'
+//       }]
+//   }
+// });
+
+class Highchart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      options: {
+        chart: {
+          type: 'pie',
+          events: {
+            drilldown: function (e) {
+              console.log("chart.events >>> drilldown e = ", e);
+            },
+            drillup: function (e) {
+              console.log("chart.events >>> drillup e = ", e);
+            }
+          }
+        },
+        title: {
+          text: 'Hao Jiang Balance:$100,000,000 January, 2020'
+        },
+        // subtitle: {
+        //     text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+        // },
+        // credits: {
+        //   enabled: true
+        // },
+        // xAxis: {
+        //   categories: ["Expenses", "Incomes"]
+        // },
+        plotOptions: {
+          series: {
+            dataLabels: {
+              enabled: true,
+              format: '{point.name}: ${point.v:.1f}, {point.y:.1f}%'
+            },
+            cursor: 'pointer',
+            // point: {
+            //   events: {
+            //     click: function (e) {
+            //       alert('clicked! ' + this.state);
+            //       console.log("plotOptions.point.events >>> ", this);
+            //     }
+            //   }
+            // }
+          }
+        },
+        tooltip: {
+          headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+          pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+        },
+        series: [
+          {
+            name: "Balance",
+            id: "Balance",
+            // colorByPoint: true,
+            data: [
+              { name: "Expenses", y: 60, v: 600, drilldown: 1},
+              { name: "Incomes", y: 40, v: 400, drilldown: 2}
+            ]
+          }
+        ],
+        drilldown: {
+          series: [
+            {
+              name: "Expenses",
+              id: 1,
+              data: [
+                { name: "Clothes", y: 70, v: 700, drilldown: 3},
+                { name: "Dining Out", y: 30, v: 300, drilldown: 4}
+              ]
+            },
+            {
+              name: "Incomes",
+              id: 2,
+              data: [
+                { name: "Salary", y: 90, v: 9000, drilldown: null},
+                { name: "Bonus", y: 10, v: 1000, drilldown: null}
+              ]
+            },
+            {
+              name: "Clothes",
+              id: 3,
+              data: [
+                { name: "Clothes1", y: 10, v: 100, drilldown: null},
+                { name: "Clothes2", y: 20, v: 200, drilldown: null}, 
+                { name: "Clothes3", y: 30, v: 300, drilldown: null},
+                { name: "Clothes4", y: 40, v: 400, drilldown: null}
+              ]
+            },
+            {
+              name: "Dining Out",
+              id: 4,
+              data: [
+                { name: "Food1", y: 20, v: 200},
+                { name: "Food2", y: 30, v: 300},
+                { name: "Food3", y: 50, v: 500}
+              ]
+            }
+          ]          
+        }
+      }
+    }
+  }
+  
+  componentDidMount() {
+    axios('/api/HomeChart')
+    .then(
+      ({data}) => {
+        this.setState({
+          loading: false,
+          options: {
+            // chart: {
+            //   type: 'pie'
+            // },
+            // title: {
+            //   text: 'WOW! Balance:$100,000,000 April, 2019'
+            // },
+            // plotOptions: {
+            //   series: {
+            //     dataLabels: {
+            //       enabled: true,
+            //       format: '{point.name}: ${point.v:.1f}, {point.y:.1f}%'
+            //     },
+            //     cursor: 'pointer',
+            //     point: {
+            //       events: {
+            //         click: function (e) {
+            //             // alert('clicked! ' + this.state);
+            //           // console.log(this);
+            //         }
+            //       }
+            //     }
+            //   }
+            // },
+            // tooltip: {
+            //   headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            //   pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            // },
+            series: data.series,
+            drilldown: data.drilldown
+          }
+        });
+      }
+    ).catch(function (error) { 
+      console.log(error);
+    });
+  }
+
+  render() {
+    console.log("render this.state.options = ", this.state.options);
+    return (
+      <div id="MajorChart" className="Hightchart">
+        <HighchartsReact highcharts={Highcharts} options={this.state.options} />
+      </div>
+    );
+  }
+}
+
+export default Highchart;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,64 +1,34 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import Highchart from './Highchart'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
-
+import drilldown from 'highcharts-drilldown';
+drilldown(Highcharts);
 
 class Home extends Component {
-
-      // 
-      constructor(props) {
-        super(props);
-        this.state = {
-          options: {
-            chart: {
-              type: 'pie'
-            },
-            credits: {
-              enabled: false
-            },
-            title: {
-              text: 'My Expenses'
-            },
-            series: [
-              { data: [1, 2, 3] }
-            ]
-          }
-        }
-      }
   
-    retrieveValues(records){
-      let outputArray = [];
-      let outputObject = {};
-      outputObject.data = records.map(x => x.value);
-      outputArray.push(outputObject);
-      return outputArray;
-    }
-  
-    getList = () => {
-      fetch('/api/getList')
-      .then(res => res.json())
-      .then(
-        ({data}) =>
-        this.setState({
-          options: {
-            series: this.retrieveValues(data)
-          }
-        })
-      );
-    }
-  
-    componentDidMount() {
-      this.getList();
-    }
-  
-
-
   render() {
+
+    const newExpenses = evt => {
+
+    };
+    const newIncomes = evt => {
+
+    };
+
     return (
     <div className="App">
       <h1>Project Home</h1>
-      <HighchartsReact highcharts={Highcharts} options={this.state.options} />
+      <div className="add_new_record_area">
+        <form onSubmit={newExpenses}>
+          <button className="add-expenses-btn" type="submit">+ New Expenses</button>
+        </form>
+        <form onSubmit={newIncomes}>
+          <button className="add-incomes-btn" type="submit">+ New Incomes</button>
+        </form>
+        </div>
+      <Highchart />
     </div>
     );
   }
