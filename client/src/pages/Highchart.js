d96f4592270b4de1efc9d2a923d55c347f6dc0db
HaoJiang0201 <haojiang0201@gmail.com>
2019-05-01 16:05:49 +0000

    Home page layout adjusted, calendar added; Highchart code cleanup

--- client/src/pages/Highchart.js
+++ client/src/pages/Highchart.js
@@ -9,95 +9,6 @@ if (!Highcharts.Chart.prototype.addSeriesAsDrilldown) {
     Drilldown(Highcharts);
 }
 
-let chartOptions = {
-  chart: {
-    type: 'pie'
-  },
-  title: {
-    text: 'Hao Jiang Balance:$100,000,000 January, 2020'
-  },
-  // subtitle: {
-  //     text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
-  // },
-  // credits: {
-  //   enabled: true`
-  // },
-  // xAxis: {
-  //   categories: ["Expenses", "Incomes"]
-  // },
-  plotOptions: {
-    series: {
-      dataLabels: {
-        enabled: true,
-        format: '{point.name}: ${point.v:.1f}, {point.y:.1f}%'
-      },
-      cursor: 'pointer',
-      point: {
-        events: {
-          click: function (e) {
-              // alert('clicked! ' + this.state);
-            // console.log(this);
-          }
-        }
-      }
-    }
-  },
-  tooltip: {
-    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-  },
-  series: [
-    {
-      name: "Balance",
-      id: "Balance",
-      // colorByPoint: true,
-      data: [
-        { name: "Expenses", y: 60, v: 600, drilldown: 1},
-        { name: "Incomes", y: 40, v: 400, drilldown: 2}
-      ]
-    }
-  ],
-  drilldown: {
-    series: [
-      {
-        name: "Expenses",
-        id: 1,
-        data: [
-          { name: "Clothes", y: 70, v: 700, drilldown: 3},
-          { name: "Dining Out", y: 30, v: 300, drilldown: 4}
-        ]
-      },
-      {
-        name: "Incomes",
-        id: 2,
-        data: [
-          { name: "Salary", y: 90, v: 9000, drilldown: null},
-          { name: "Bonus", y: 10, v: 1000, drilldown: null}
-        ]
-      },
-      {
-        name: "Clothes",
-        id: 3,
-        data: [
-          { name: "Clothes1", y: 10, v: 100, drilldown: null},
-          { name: "Clothes2", y: 20, v: 200, drilldown: null},
-          { name: "Clothes3", y: 30, v: 300, drilldown: null},
-          { name: "Clothes4", y: 40, v: 400, drilldown: null}
-        ]
-      },
-      {
-        name: "Dining Out",
-        id: 4,
-        data: [
-          { name: "Food1", y: 20, v: 200},
-          { name: "Food2", y: 30, v: 300},
-          { name: "Food3", y: 50, v: 500}
-        ]
-      }
-    ]
-  }
-};
-
 class Highchart extends Component {
   constructor(props) {
     super(props);
@@ -242,8 +153,8 @@ class Highchart extends Component {
   componentDidMount() {
     axios('/api/HomeChart', {
       params: {
-        start: "2019-01-01",
-        end: "2019-04-30"
+        // start: "2019-01-01",
+        // end: "2019-04-30"
       }
     })
     .then(
