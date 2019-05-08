ce8b1fff7758dc9788c27770b949fea062e389b0
boksul <boksul@hotmail.com>
2019-05-08 16:42:20 +0000

    final check

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -31,7 +31,7 @@ class Navbar extends Component {
               <a>Write Your Own BudgeStory</a>
             </div>
           <div className="userInfo">
-            <a>Howard</a>
+            <a>Brendan</a>
           </div>
       </nav>
     )
